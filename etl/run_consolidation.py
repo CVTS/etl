6fe483cd10a58069b8e6a9e17474aaa4f47af838
Pedro Camargo <c@margo.co>
2021-09-02 21:26:45 +1000

    Minor improvements

--- etl/run_consolidation.py
+++ etl/run_consolidation.py
@@ -8,8 +8,6 @@ def main():
     for month in range(6, 7):
         cwm = ConsolidateWholeMonth(month, num_workers)
         cwm.consolidate()
-        # scp -P 2235 run_consolidation.py consolidate_month.py ../requirements.txt ubuntu@103.160.90.151:/mnt/data_lake/src
-
 
 if __name__ == '__main__':
     main()
